e4ecc353084211349abb2b56e4aa831caeb4b852
Adhir DC <adhir.serrano@gmail.com>
2024-08-03 13:50:43 -0600

    Refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,48 @@
 const button = document.getElementById('button');
-const img    = document.getElementById('img');
-const text   = document.getElementById('info');
-const url    = document.getElementById('pokeLogo');
+const img = document.getElementById('img');
+const text = document.getElementById('info');
+const url = document.getElementById('pokeLogo');
+const searchBox = document.getElementById('searchBox');
 
 url.setAttribute('src', 'assets/img/logo2.png');
-
 img.style.display = 'none';
 
-button.addEventListener('click',()=>{
-    const inputSearch = document.getElementById('searchBox').value
-    const request = new XMLHttpRequest()
+// Function to validate input value
+function validateInput(inputValue) {
+    const number = parseInt(inputValue);
+    if (isNaN(number) || number < 1 || number > 1025) {
+        Swal.fire("Choose between 1 and 1025", "", "warning");
+        searchBox.value = '';
+        return false;
+    }
+    return true;
+}
 
-    document.getElementById('searchBox').addEventListener('input', function() {
-        let inputValue = parseInt(this.value);
-    
-        if (isNaN(inputValue) || inputValue < 1 || inputValue > 1025) {
-            Swal.fire("Choose between 1 and 1025","","warning");
-            this.value = '';
+// Function to fetch Pokémon data
+async function fetchPokemonData(pokemonId) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+        if (!response.ok) {
+            Swal.fire("No response found", "", "error");
         }
-    });
+        const data = await response.json();
+        img.style.display = '';
+        img.src = data.sprites.front_default;
+        text.textContent = data.name.toUpperCase();
+    } catch (error) {
+        Swal.fire("There was a network error", "", "error");
+    }
+}
 
-    request.open('GET',`https://pokeapi.co/api/v2/pokemon/${inputSearch}`)
+// Event Listener for button click
+button.addEventListener('click', () => {
+    const inputSearch = searchBox.value;
+    if (validateInput(inputSearch)) {
+        fetchPokemonData(inputSearch);
+    }
+});
 
-    request.onload = () => {
-        if(request.status == 200){
-            const datoPokemon = JSON.parse( request.responseText)
-            img.style.display = '';
-            img.src = `${datoPokemon.sprites.front_default}`;
-            text.textContent = datoPokemon.name.toUpperCase();
-        } else {
-            Swal.fire("Choose between 1 and 1025","","warning");
-        }
-    };
-
-    request.onerror = () => {
-        Swal.fire("There was a network error","","error");
-    };
-
-    request.send();
-})
+// Event Listener for input validation
+searchBox.addEventListener('input', () => {
+    validateInput(searchBox.value);
+});
